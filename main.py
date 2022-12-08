f10864f31bb3a3763e13cf92aabdc5c41e7713b5
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-08 12:40:37 +0530

    Update main.py (define redirect_to_doc)

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-def app(event):
-    return "Hello, world!"
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+# Create app instance 
+app = FastAPI(
+    title="SongAPI"
+)
+
+# ROOT 
+@app.get('/')
+def redirect_to_doc():
+    # should be redirect to api documention
+    return RedirectResponse('/docs')
+
+
