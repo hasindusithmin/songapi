eb3e04739714ef1874d695a692b256158f45b3ef
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-08 13:09:18 +0530

    Update main.py (define find_all)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import json
+from enum import Enum
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
@@ -13,4 +14,14 @@ def redirect_to_doc():
     # should be redirect to api documention
     return RedirectResponse('/docs')
 
+class Keys(Enum):
+    SONG = 'song'
+    SINGER = 'singer'
+
+# route:/find-all 
+@app.get('/find-all')
+async def find_all(sort:Keys=Keys.SONG,reverse:bool=False):
+    with open('resource.json','r') as fp:
+        resource =  json.load(fp)
+        return sorted(resource,key=lambda k:k[sort.value],reverse=reverse)
 
